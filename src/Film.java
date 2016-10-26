d4df7ee42fa5bc904315f2591308a5eac4991efc
jstefanowiczczop <jstefanowicz.czop@gmail.com>
2016-10-26 20:30:11 +0200

    hiuyiuh

---
+++ src/Film.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Jola on 2016-10-26.
+ */
+public class Film {
+   String tytul;
+    String rodzaj;
+    int ocena;
+
+
+    void odtworz()
+        {
+        System.out.println("Odtwarzamy film.");
+
+    }
+}
+
+
---
+++ src/FilmTester.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Jola on 2016-10-26.
+ */
+public class FilmTester {
+
+    public static void main (String[] args){
+        Film pierwszy = new Film();
+        pierwszy.tytul = "Przeminęło  z hossą";
+        pierwszy.rodzaj = "triler";
+        pierwszy.ocena = 2;
+        Film drugi =  new Film ();
+        drugi.tytul = "bla bla bla";
+        drugi.rodzaj = "sensacja";
+                drugi.ocena = 1;
+        Film trzeci = new Film ();
+        trzeci.tytul = "gdzie sa klucze";
+                trzeci.rodzaj = "poradnik";
+        trzeci.ocena = 5;
+
+
+
+
+
+    }
+
+
+
+
+}
